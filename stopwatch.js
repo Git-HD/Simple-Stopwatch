7bca68875ae5c2cb1b7f8d789c7a2a098b82d44f
Henrique <henrique.leite@unyleya.com.br>
2022-02-12 10:33:30 -0300

    Simple Web Stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,40 @@
+let [millisseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let display = document.getElementById('output');
+let int;
+
+var toggle = true;
+
+document.getElementById('toggle').addEventListener('click', () => {
+    if (toggle) {
+        int = setInterval(toggleCounting, 10);
+        toggle = !toggle
+    } else {
+        toggle = !toggle
+        clearInterval(int);
+        [millisseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+        display.innerHTML = '00 : 00 : 00 : 000';
+    }
+});
+
+toggleCounting = () => {
+    millisseconds += 10;
+    if (millisseconds == 1000) {
+        millisseconds = 0;
+        seconds++;
+        if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+            if (minutes == 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = millisseconds < 10  ? "00" + millisseconds : millisseconds < 100 ? "0" + millisseconds : millisseconds;
+
+    display.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
+}
